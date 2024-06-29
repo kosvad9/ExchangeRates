c61351bbe8ca95cf581fb412b8698ff1781b08bb
Vadim <kosvad9@gmail.com>
2024-06-29 10:41:56 +0300

    Initial commit

---
+++ src/test/java/com/kosvad9/ExchangeRates/ExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosvad9.ExchangeRates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
